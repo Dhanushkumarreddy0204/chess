ebe8c23a0aea3f02303bfd13e3103ba89052e725
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-02-25 21:41:10 +0530

    add initial arbiter object structure in arbiter.js

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -1,3 +1,8 @@
 import { areSameColorTiles, findPieceCoords } from '../helper';
 import { getKnightMoves, getRookMoves, getBishopMoves, getQueenMoves, getKingMoves, getPawnMoves, getPawnCaptures, getCastlingMoves, getPieces, getKingPosition } from './getMoves'
 import { movePiece,movePawn } from './move';
+
+const arbiter = {
+
+
+    },
