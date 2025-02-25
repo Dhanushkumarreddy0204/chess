102e71fd2997ff86db9fa51001177febc75f1f88
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-02-25 21:46:51 +0530

    refactor arbiter.js to clean up imports and remove commented-out code

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -1,9 +1,4 @@
 import { areSameColorTiles, findPieceCoords } from '../helper';
-import { getKnightMoves, getRookMoves, getBishopMoves, getQueenMoves, getKingMoves, getPawnMoves, getPawnCaptures, getCastlingMoves, getPieces, getKingPosition } from './getMoves'
-import { movePiece,movePawn } from './move';
+import { getKnightMoves, getRookMoves, getBishopMoves, getQueenMoves, getKingMoves, getPawnMoves, getPawnCaptures, getCastlingMoves, getPieces, getKingPosition } from './getMoves';
+import { movePiece, movePawn } from './move';
 
-const arbiter = {
-
-    getRegularMoves : function ({position,piece,rank,file}) {
-    },
-    },
