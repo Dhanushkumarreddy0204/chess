99e1048ad1c95497d2843100a281fc990ac0b151
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-02-26 14:24:56 +0530

    add getRegularMoves method to arbiter for knight move retrieval

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -2,3 +2,10 @@ import { areSameColorTiles, findPieceCoords } from '../helper';
 import { getKnightMoves, getRookMoves, getBishopMoves, getQueenMoves, getKingMoves, getPawnMoves, getPawnCaptures, getCastlingMoves, getPieces, getKingPosition } from './getMoves'
 import { movePiece,movePawn } from './move';
 
+const arbiter = {
+
+    getRegularMoves : function ({position,piece,rank,file}) {
+        if (piece.endsWith('n'))
+            return getKnightMoves({position,rank,file});
+    },
+}
