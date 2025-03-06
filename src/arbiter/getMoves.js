848e2199191d6635f02a2b5f2ce72353ef7d9e3b
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-03-06 21:39:07 +0530

    add logic to iterate through directions for rook move calculation

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -12,4 +12,7 @@ export const getRookMoves = ({position,piece,rank,file}) => {
         [0,1],
     ]
 
-    
+    direction.forEach(dir => {
+        for (let i = 1; i <= 8; i++) {
+            const x = rank+(i*dir[0])
+            const y = file+(i*dir[1])
