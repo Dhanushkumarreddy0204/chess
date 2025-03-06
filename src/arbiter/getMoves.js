9fc16e7e91f465115952c09c972db85ee83c9fcc
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-03-06 21:38:45 +0530

    add direction array to getRookMoves for move calculation

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -4,3 +4,12 @@ export const getRookMoves = ({position,piece,rank,file}) => {
     const moves = []
     const us = piece[0]
     const enemy = us === 'w' ? 'b' : 'w'
+
+    const direction = [
+        [-1,0],
+        [1,0],
+        [0,-1],
+        [0,1],
+    ]
+
+    
