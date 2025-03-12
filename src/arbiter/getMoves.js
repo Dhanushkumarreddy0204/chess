82ecef0a15a45ec36884a9ce6398d4cf99c8e10c
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-03-12 21:20:34 +0530

    add getPawnMoves function to calculate possible moves for the pawn

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -111,3 +111,7 @@ export const getKingMoves = ({position,piece,rank,file}) => {
     })
     return moves
 }
+export const getPawnMoves = ({position,piece,rank,file}) => {
+
+    const moves = []
+    const dir = piece==='wp' ? 1 : -1
