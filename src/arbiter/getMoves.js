062afebdc862fed3d6f4dfcde0701715fee786ce
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-03-13 22:15:12 +0530

    add direction and enemy piece logic to getPawnCaptures function

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -131,3 +131,5 @@ export const getPawnMoves = ({position,piece,rank,file}) => {
 export const getPawnCaptures =  ({position,prevPosition,piece,rank,file}) => {
 
     const moves = []
+     const dir = piece==='wp' ? 1 : -1
+    const enemy = piece[0] === 'w' ? 'b' : 'w'
