70fe07e162aa68b1d474b79aa200839237649e8b
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-03-15 22:14:17 +0530

    add logic for capturing enemy pawns with adjacent files in getPawnCaptures function

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -145,3 +145,19 @@ export const getPawnCaptures =  ({position,prevPosition,piece,rank,file}) => {
 
     const enemyPawn = dir === 1 ? 'bp' : 'wp'
     const adjacentFiles = [file-1,file+1]
+    if(prevPosition){
+        if ((dir === 1 && rank === 4) || (dir === -1 && rank === 3)){
+            adjacentFiles.forEach(f => {
+                if (position?.[rank]?.[f] === enemyPawn && 
+                    position?.[rank+dir+dir]?.[f] === '' &&
+                    prevPosition?.[rank]?.[f] === '' && 
+                    prevPosition?.[rank+dir+dir]?.[f] === enemyPawn){
+                        moves.push ([rank+dir,f])
+                    }
+            })
+        }
+    }
+
+
+    return moves
+}
