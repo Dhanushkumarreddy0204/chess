55eef89b7e42c86ad1d2d12f642e53c6a36c4693
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-03-20 21:23:21 +0530

    add condition to handle castling moves for right direction

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -231,3 +231,4 @@ export const getCastlingMoves = ({position,castleDirection,piece,rank,file}) => 
             })){
                 moves.push ([7,2])
             }
+            if (['right','both'].includes(castleDirection)
