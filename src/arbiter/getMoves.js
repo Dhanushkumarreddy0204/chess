1c2c4041cd1e767e78918351e76306ed2fb81348
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-03-23 00:11:34 +0530

    add getPieces function to collect enemy pieces from the board position

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -291,3 +291,13 @@ export const getPieces = (position, enemy) => {
     const enemyPieces = []
     position.forEach((rank,x) => {
         rank.forEach((file, y) => {
+            if(position[x][y].startsWith(enemy))
+                enemyPieces.push({
+                    piece : position[x][y],
+                    rank : x,
+                    file : y,
+                })
+        })
+    })
+    return enemyPieces
+}
