37c0cfad5b7a9457822e7f97c433ea1dfed64e7e
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-04-14 12:45:10 +0530

    fix: add missing newline at the end of MovesList.js for consistent formatting

--- src/components/control/bits/MovesList.js
+++ src/components/control/bits/MovesList.js
@@ -4,3 +4,13 @@ import './MovesList.css'
 const MovesList = () => {
 
     const { appState : {movesList} } = useAppContext();
+
+    
+    return <div className='moves-list'>
+        {movesList.map((move,i) => 
+            <div key={i} data-number={Math.floor(i/2)+1}>{move}</div>
+        )}
+    </div>
+}
+
+export default MovesList
