7e73d4aab8eb392c0b327ff01f1c71b425cee96b
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-04-14 12:45:28 +0530

    fix: remove unnecessary whitespace in MovesList.js for consistent formatting

--- src/components/control/bits/MovesList.js
+++ src/components/control/bits/MovesList.js
@@ -5,7 +5,6 @@ const MovesList = () => {
 
     const { appState : {movesList} } = useAppContext();
 
-    
     return <div className='moves-list'>
         {movesList.map((move,i) => 
             <div key={i} data-number={Math.floor(i/2)+1}>{move}</div>
