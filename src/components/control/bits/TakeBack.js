7488a33b13caab3d748b304445ab3fd7a76a4a82
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-04-15 21:34:14 +0530

    fix: add missing newline at the end of TakeBack.js and remove unnecessary whitespace

--- src/components/control/bits/TakeBack.js
+++ src/components/control/bits/TakeBack.js
@@ -4,8 +4,10 @@ import { takeBack } from '../../../reducer/actions/move';
 const TakeBack = () => {
 
     const { dispatch } = useAppContext();
-    
+
     return <div>
         <button onClick={() => dispatch(takeBack())}>Take Back</button>
     </div>
 }
+
+export default TakeBack
