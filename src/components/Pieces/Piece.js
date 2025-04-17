b7d38ddcdf6d3b76f5bc43e5e43a8efa9a0db331
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-04-17 16:59:19 +0530

    fix: add missing import statement for generateCandidates in Piece.js

--- src/components/Pieces/Piece.js
+++ src/components/Pieces/Piece.js
@@ -1,3 +1,11 @@
 import arbiter from '../../arbiter/arbiter';
 import { useAppContext }from '../../contexts/Context'
 import { generateCandidates } from '../../reducer/actions/move';
+
+const Piece = ({
+    rank,
+    file,
+    piece,
+})  => {
+
+    const { appState, dispatch } = useAppContext();
