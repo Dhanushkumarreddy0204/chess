a10b27108e5fcb579ffb5ff7b2013c9d70105448
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-04-27 19:04:39 +0530

    fix: add missing imports for move actions and helper functions in Pieces.js

--- src/components/Pieces/Pieces.js
+++ src/components/Pieces/Pieces.js
@@ -5,3 +5,8 @@ import { useAppContext }from '../../contexts/Context'
 import { openPromotion } from '../../reducer/actions/popup'
 import { getCastlingDirections } from '../../arbiter/getMoves'
 import { updateCastling, detectStalemate, detectInsufficientMaterial, detectCheckmate} from '../../reducer/actions/game'
+
+import { makeNewMove, clearCandidates } from '../../reducer/actions/move'
+import arbiter from '../../arbiter/arbiter'
+import { getNewMoveNotation } from '../../helper'
+
