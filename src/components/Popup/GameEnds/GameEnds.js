e4ee98ba100cc6ee1915d7f0bf8de72eb79ba6f1
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-04 22:02:32 +0530

    feat: add GameEnds component definition in GameEnds.js

--- src/components/Popup/GameEnds/GameEnds.js
+++ src/components/Popup/GameEnds/GameEnds.js
@@ -2,3 +2,5 @@ import { Status } from '../../../constants';
 import { useAppContext }from '../../../contexts/Context'
 import { setupNewGame } from '../../../reducer/actions/game';
 import './GameEnds.css'
+
+const GameEnds = ({onClosePopup})
