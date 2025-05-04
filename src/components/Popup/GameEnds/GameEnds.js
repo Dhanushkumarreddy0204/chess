5c58199eb8926cd79c6f7065e7bf9b5e9853e91f
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-04 22:02:19 +0530

    feat: add useAppContext and setupNewGame imports in GameEnds.js

--- src/components/Popup/GameEnds/GameEnds.js
+++ src/components/Popup/GameEnds/GameEnds.js
@@ -1,2 +1,4 @@
 import { Status } from '../../../constants';
 import { useAppContext }from '../../../contexts/Context'
+import { setupNewGame } from '../../../reducer/actions/game';
+import './GameEnds.css'
