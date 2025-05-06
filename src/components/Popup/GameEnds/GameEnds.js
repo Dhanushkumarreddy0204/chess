a592c1eee02db0ddbb3775cfc4bf7b694df24246
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-06 15:39:20 +0530

    fix: correct rendering structure in GameEnds component

--- src/components/Popup/GameEnds/GameEnds.js
+++ src/components/Popup/GameEnds/GameEnds.js
@@ -6,22 +6,23 @@ import './GameEnds.css'
 const GameEnds = ({onClosePopup}) => {
 
     const { appState : {status} , dispatch } = useAppContext();
-
+    
     if (status === Status.ongoing || status === Status.promoting)
         return null
 
     const newGame = () => {
         dispatch(setupNewGame())
     }
+
     const isWin = status.endsWith('wins')
 
-    return <div className="popup--inner popup--inner__center"></div>
-    <h1>{isWin ? status : 'Draw'}</h1>
-    <p>{!isWin && status}</p>
+    return <div className="popup--inner popup--inner__center">
+        <h1>{isWin ? status : 'Draw'}</h1>
+        <p>{!isWin && status}</p>
         <div className={`${status}`}/>
         <button onClick={newGame}>New Game</button>
     </div>
-       
+   
 }
 
 export default GameEnds
