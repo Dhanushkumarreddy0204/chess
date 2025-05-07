b756045cd5a0b4937142376e8b1d5b84c13c2f2a
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-08 00:02:33 +0530

    fix: ensure consistent formatting in PromotionBox component

--- src/components/Popup/PromotionBox/PromotionBox.js
+++ src/components/Popup/PromotionBox/PromotionBox.js
@@ -2,3 +2,7 @@ import { useAppContext } from "../../../contexts/Context";
 import { copyPosition, getNewMoveNotation } from "../../../helper";
 import { makeNewMove, clearCandidates } from "../../../reducer/actions/move";
 import "./PromotionBox.css";
+
+const PromotionBox = ({ onClosePopup }) => {
+    const { appState, dispatch } = useAppContext();
+    const { promotionSquare } = appState;
