eecbeeb9a1ccfeceec16f2d6b5889c325475e443
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-08 00:02:10 +0530

    fix: reformat import statements in PromotionBox component

--- src/components/Popup/PromotionBox/PromotionBox.js
+++ src/components/Popup/PromotionBox/PromotionBox.js
@@ -1,1 +1,4 @@
 import { useAppContext } from "../../../contexts/Context";
+import { copyPosition, getNewMoveNotation } from "../../../helper";
+import { makeNewMove, clearCandidates } from "../../../reducer/actions/move";
+import "./PromotionBox.css";
