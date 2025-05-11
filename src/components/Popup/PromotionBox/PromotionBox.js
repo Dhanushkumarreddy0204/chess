b2d75397eab1d3403c8f15b60ea1cd75e00c5d9c
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-12 00:12:26 +0530

    fix: add newline for better formatting in PromotionBox component

--- src/components/Popup/PromotionBox/PromotionBox.js
+++ src/components/Popup/PromotionBox/PromotionBox.js
@@ -52,7 +52,8 @@ const PromotionBox = ({ onClosePopup }) => {
     dispatch(makeNewMove({ newPosition, newMove }));
   };
 
-  return <div
+  return (
+    <div
       className="popup--inner promotion-choices"
       style={getPromotionBoxPosition()}
     >
