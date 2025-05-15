64e5ab43a864d784189b78ce508b8da34d0d5d93
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-16 00:10:52 +0530

    fix: reorder import statements in Popup component for consistency

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { Status } from '../../constants';
 import { useAppContext }from '../../contexts/Context'
 import { closePopup } from '../../reducer/actions/popup';
+
+import './Popup.css'
