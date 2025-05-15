bf392570095ffc1b5d3724f09cf86b599f78a1ee
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-16 00:10:41 +0530

    fix: add missing imports for useAppContext and closePopup in Popup component

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,2 +1,4 @@
 import React from 'react';
 import { Status } from '../../constants';
+import { useAppContext }from '../../contexts/Context'
+import { closePopup } from '../../reducer/actions/popup';
