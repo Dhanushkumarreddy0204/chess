56b4ca11c708330742cb3ac271fa7aa12c9da4f0
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-05-28 12:30:18 +0330

    fix: add newline at end of file and format openPromotion action in popup.js

--- src/reducer/actions/popup.js
+++ src/reducer/actions/popup.js
@@ -2,3 +2,6 @@ import actionTypes from '../actionTypes';
 export const openPromotion = ({rank,file,x,y}) =>{
     return {
         type: actionTypes.PROMOTION_OPEN,
+        payload: {rank,file,x,y}
+    }
+}
