4142e6ea7a403bb910ff6ddd8141fd8bfa4501aa
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-06-16 09:40:40 +0530

    fix: add import statement for Control component in App.js

--- src/App.js
+++ src/App.js
@@ -4,3 +4,4 @@ import { reducer } from './reducer/reducer'
 import { useReducer } from 'react'
 import { initGameState } from './constants';
 import AppContext from './contexts/Context'
+import Control from './components/Control/Control';
