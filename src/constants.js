b75c888fe4adca016a5ba9c7cec46168916cc15f
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-06-21 09:43:40 +0530

    fix: add missing entries to Status constant in constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,5 @@ export const Status = {
     'ongoing' : 'Ongoing',
     'promoting' : 'Promoting',
     'white' : 'White wins',
+    'black' : 'Black wins',
+    'stalemate' : 'Game draws due to stalemate',
