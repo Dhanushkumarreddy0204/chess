86598129d5d69962e67b3709248dffa259f9419f
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-06-23 09:41:43 +0530

    fix: implement createPosition function in helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,3 @@
 export const getCharacter = (file) => String.fromCharCode(file + 96);
-export const createPosition = () => 
+export const createPosition = () => {
+  const position = new Array(8).fill("").map((x) => new Array(8).fill(""));
