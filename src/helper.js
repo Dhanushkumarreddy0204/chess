2a4f37854b8306a0ab3a469bcc25a066eac8d00b
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-06-23 09:42:18 +0530

    fix: add newline at the end of createPosition function in helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
 export const getCharacter = (file) => String.fromCharCode(file + 96);
 export const createPosition = () => {
   const position = new Array(8).fill("").map((x) => new Array(8).fill(""));
-for (let i = 0; i < 8; i++)
+  for (let i = 0; i < 8; i++)
