2c6271185a5fa07d0e9a8b6d16a8070a26b92ea1
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-06-27 09:34:41 +0530

    feat: add findPieceCoords function to helper.js

--- src/helper.js
+++ src/helper.js
@@ -39,3 +39,4 @@ export const copyPosition = (position) => {
 export const areSameColorTiles = (coords1, coords2) =>
   (coords1.x + coords1.y) % 2 === coords2.x + coords2.y;
 
+export const findPieceCoords = (position, type) 
