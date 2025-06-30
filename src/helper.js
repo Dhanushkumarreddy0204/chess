8a8f8fb33437584d8d54d33299c804e3cca5c4d4
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-06-30 09:44:52 +0530

    fix: format code for consistency in helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,8 @@
 export const getCharacter = (file) => String.fromCharCode(file + 96);
 export const createPosition = () => {
   const position = new Array(8).fill("").map((x) => new Array(8).fill(""));
-  for (let i = 0; i < 8; i++){
+
+  for (let i = 0; i < 8; i++) {
     position[6][i] = "bp";
     position[1][i] = "wp";
   }
@@ -14,6 +15,7 @@ export const createPosition = () => {
   position[0][5] = "wb";
   position[0][6] = "wn";
   position[0][7] = "wr";
+
   position[7][0] = "br";
   position[7][1] = "bn";
   position[7][2] = "bb";
@@ -28,6 +30,7 @@ export const createPosition = () => {
 
 export const copyPosition = (position) => {
   const newPosition = new Array(8).fill("").map((x) => new Array(8).fill(""));
+
   for (let rank = 0; rank < position.length; rank++) {
     for (let file = 0; file < position[0].length; file++) {
       newPosition[rank][file] = position[rank][file];
@@ -36,6 +39,7 @@ export const copyPosition = (position) => {
 
   return newPosition;
 };
+
 export const areSameColorTiles = (coords1, coords2) =>
   (coords1.x + coords1.y) % 2 === coords2.x + coords2.y;
 
@@ -45,7 +49,7 @@ export const findPieceCoords = (position, type) => {
     rank.forEach((pos, j) => {
       if (pos === type) results.push({ x: i, y: j });
     });
-    });
+  });
   return results;
 };
 
@@ -59,22 +63,25 @@ export const getNewMoveNotation = ({
   promotesTo,
 }) => {
   let note = "";
-  
+
   rank = Number(rank);
   file = Number(file);
   if (piece[1] === "k" && Math.abs(file - y) === 2) {
     if (file < y) return "O-O";
     else return "O-O-O";
   }
-   if (piece[1] !== "p") {
+
+  if (piece[1] !== "p") {
     note += piece[1].toUpperCase();
     if (position[x][y]) {
       note += "x";
     }
-    } else if (rank !== x && file !== y) {
+  } else if (rank !== x && file !== y) {
     note += getCharacter(file + 1) + "x";
   }
+
   note += getCharacter(y + 1) + (x + 1);
+
   if (promotesTo) note += "=" + promotesTo.toUpperCase();
 
   return note;
